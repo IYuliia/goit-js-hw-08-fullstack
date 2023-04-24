29cb41de4b848a88a1751003347df26a18d692c5
IYuliia <catnew2012@gmail.com>
2023-04-25 00:55:46 +0200

    added feedback requirements

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', throttle(onInputChange, 500));
+window.addEventListener('load', onLoad);
+form.addEventListener('submit', onFormSubmit);
+
+function onInputChange() {
+  const savedData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+  const savedDataJSON = JSON.stringify(savedData);
+  localStorage.setItem(STORAGE_KEY, savedDataJSON);
+}
+
+function onLoad() {
+  const savedStateJSON = localStorage.getItem(STORAGE_KEY);
+  if (savedStateJSON) {
+    const savedState = JSON.parse(savedStateJSON);
+    form.email.value = savedState.email;
+    form.message.value = savedState.message;
+  }
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const savedData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+
+  console.log(savedData);
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
